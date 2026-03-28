ba4fd54d153b62c0fb5e1d26e7afde04cf6255b8
nehakhemchandani <nehakhemchandani@nehakhemchandani.local>
2026-03-28 23:34:58 +0530

    Created Express Server

---
+++ src/app.js
@@ -0,0 +1,27 @@
+//console.log("Staring Express Server!!");
+
+const express = require("express"); // express module of node_modules
+const app = express(); // creating express js applicaton or instance
+
+// app.use((req,res) =>{   // Handle Request and Response  
+//     res.send("Hello From the Server!!!");
+// });
+
+app.use("/test",(req,res) =>{   // Handle Request and Response  
+    res.send("Hello From the Server!!!");
+});
+
+app.use("/dashboard",(req,res) =>{   // Handle Request and Response  
+    res.send("Dashboard Devlopers Tinder!!!");
+});
+
+app.use("/hello",(req,res) =>{   // Handle Request and Response  
+    res.send("Hello from Neha Khemchandani !!!");
+});
+
+
+app.listen(3002, ()=>{
+    console.log("Server is listening on port 3002...");
+
+});   // listen to incoming requests .i.e created server and listening on 3002 port
+ 
