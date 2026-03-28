f1500a31db1782953314d0754a3545a5007284f3
nehakhemchandani <nehakhemchandani@nehakhemchandani.local>
2026-03-29 00:21:42 +0530

    Created Express Server

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const app = express(); // creating express js applicaton or instance
 //     res.send("Hello From the Server!!!");
 // });
 
+// app.use("/",(req,res) =>{   // Handle Request and Response  
+//     res.send("Hi Gannu Bha!!!");
+// });
+
 app.use("/test",(req,res) =>{   // Handle Request and Response  
     res.send("Hello From the Server!!!");
 });
@@ -19,6 +23,9 @@ app.use("/hello",(req,res) =>{   // Handle Request and Response
     res.send("Hello from Neha Khemchandani !!!");
 });
 
+app.use("/",(req,res) =>{   // Handle Request and Response  
+    res.send("Hi !!!");
+});
 
 app.listen(3002, ()=>{
     console.log("Server is listening on port 3002...");
